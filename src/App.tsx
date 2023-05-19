ef3227b2c4940357124de95fc92e4e9f3bb06d75
Nkuna William <64930245+NK-William@users.noreply.github.com>
2023-05-19 14:52:01 +0200

    Minor updates

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,8 @@ function App() {
   return (
     <div className="App">
       {/* <Home /> */}
-      {/* <AboutUs /> */}
-      <PrivacyPolicy />
+      <AboutUs />
+      {/* <PrivacyPolicy /> */}
     </div>
   );
 }
