8014bb2b9a6ae99d7e8c8e8ccf32e346eead7247
Nkuna William <64930245+NK-William@users.noreply.github.com>
2023-06-12 21:42:26 +0200

    Entry component value

--- src/components/entry/Entry.tsx
+++ src/components/entry/Entry.tsx
@@ -5,17 +5,25 @@ interface IEntry {
   title: string;
   showRequired?: boolean;
   value?: string;
+  onTextChange: (text: string) => void;
+  error?: boolean;
 }
 
-function Entry({ title, showRequired, value }: IEntry) {
+function Entry({ title, showRequired, value, onTextChange, error }: IEntry) {
+
+  const handleTextChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    onTextChange(e.target.value);
+  }
+
   return (
     <div className="entry__container">
       <p className="entry__title">
         {title}
         <span>{showRequired && "(required)"}</span>
       </p>
-      <input type="text" onChange={() => {}} />
-      {/* <p className="entry__error-text">Name is required</p> */}
+      <input type="text" value={value} onChange={handleTextChange} />
+      {error &&<p className="entry__error-text">A valid {title.toLocaleLowerCase()} is required</p>}
     </div>
   );
 }
--- src/containers/quoteForm/QuoteForm.tsx
+++ src/containers/quoteForm/QuoteForm.tsx
@@ -1,24 +1,92 @@
-import React, { FormEvent } from "react";
+import React, { useState } from "react";
 import "./quoteForm.css";
 import { Entry, Button } from "../../components";
 
 const QuoteForm = () => {
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+
+  const [name, setName] = useState("");
+  const [surname, setSurname] = useState("");
+  const [email, setEmail] = useState("");
+  const [contactNumber, setContactNumber] = useState("");
+  const [message, setMessage] = useState("");
+
+  const [nameError, setNameError] = useState(false);
+  const [surnameError, setSurnameError] = useState(false);
+  const [emailError, setEmailError] = useState(false);
+  const [contactNumberError, setContactNumberError] = useState(false);
+  const [messageError, setMessageError] = useState(false);
+
+  const handleTextChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     e.preventDefault();
-    console.log("Submitted");
+    setMessage(e.target.value);
+  }
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if(isValid()){
+      // submit
+      console.log("name: ",name);
+      console.log("surname: ",surname);
+      console.log("email: ",email);
+      console.log("contactNumber: ",contactNumber);
+      console.log("message: ",message);
+    }else{
+      return;
+    }
+
   };
+
+  const isValid= () => {
+    let valid = true;
+    if(name.trim() === ""){
+      setNameError(true);
+      valid = false;
+    }else{
+      setNameError(false);
+    }
+
+    if(surname.trim() === ""){
+      setSurnameError(true);
+      valid = false;
+    }else{
+      setSurnameError(false);
+    }
+
+    if(email.trim() === ""){
+      setEmailError(true);
+      valid = false;
+    }else{
+      setEmailError(false);
+    }
+
+    if(contactNumber.trim() === ""){
+      setContactNumberError(true);
+      valid = false;
+    }else{
+      setContactNumberError(false);
+    }
+
+    if(message.trim() === ""){
+      setMessageError(true);
+      valid = false;
+    }else{
+      setMessageError(false);
+    }
+
+    return valid;
+  }
 
   return (
     <form className="quoteForm__container" onSubmit={handleSubmit}>
-      <Entry title="Name" value="" showRequired />
-      <Entry title="Surname" value="" showRequired />
-      <Entry title="Email" value="" showRequired />
-      <Entry title="Contact number" value="" showRequired />
-      <Entry title="Subject" value="" />
+      <Entry title="Name" value={name} onTextChange={setName} showRequired error={nameError}/>
+      <Entry title="Surname" value={surname} showRequired onTextChange={setSurname} error={surnameError}/>
+      <Entry title="Email" value={email} showRequired onTextChange={setEmail} error={emailError}/>
+      <Entry title="Contact number" value={contactNumber} showRequired onTextChange={setContactNumber} error={contactNumberError}/>
       <div className="quoteForm__text-area">
         <p className="quoteForm__message-title">Message</p>
-        <textarea onChange={() => {}} />
-        {/* <p className="entry__error-text">Message is required</p> */}
+        <textarea value={message} onChange={handleTextChange} />
+        {messageError && <p className="entry__error-text">Message is required</p>}
       </div>
       <Button text="SUBMIT" />
     </form>
